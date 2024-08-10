69e161dd2d8cc81f5a5a1c0999e3f2434a17968a
Ehren Abbott <era12358@gmail.com>
2024-08-10 15:09:09 -0500

    Create onDrop arrow funtion in Dropzone component

--- src/Dropzone.js
+++ src/Dropzone.js
@@ -1,8 +1,20 @@
 import React from 'react'; 
 import { useDropzone } from 'react-dropzone';
 
-function Dropzone(){ 
-    
+const Dropzone = () => { 
+    function onDrop(acceptedFiles){ 
+        console.log(acceptedFiles)
+    }
+
+    const { getRootProps, getInputProps } = useDropzone({  onDrop });
+
+
+
+    return ( 
+        <div> 
+            <h1>dropzone component</h1>
+        </div>
+    )
 }
 
 export default Dropzone;
