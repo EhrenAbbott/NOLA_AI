767c0b2441b3fb6ce343dcff069804cc21854f5c
Ehren Abbott <era12358@gmail.com>
2024-08-11 15:48:10 -0500

    Create modal element with error message and close button

--- src/ErrorModal.js
+++ src/ErrorModal.js
@@ -1,7 +1,21 @@
 import React from 'react'; 
 import Modal from 'react-modal';
 
+Modal.setAppElement('#root')
 
+const ErrorModal = ({isOpen, onRequestClose, message}) => { 
+    return( 
+        <Modal
+            isOpen={isOpen}
+            onRequestClose={onRequestClose}
+            contentLabel="Error Message"
+            className='error-modal'
+        >
+            <h2>{message}</h2>
+            <button onClick={onRequestClose}>Close</button>
+        </Modal>
+    )
+}
 
 
 export default ErrorModal
